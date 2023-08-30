8415141e7b76ea495bb2885715026c2519b91475
latiniuxs <koredeoladetohun@gmail.com>
2023-08-30 04:52:51 +0100

    .

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,79 +1,117 @@
-import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import './ContactMe.css';
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import "./ContactMe.css";
 
 const ContactMe = () => {
   const initialValues = {
-    name: '',
-    email: '',
-    message: '',
+    name: "",
+    email: "",
+    message: "",
   };
 
   const validationSchema = Yup.object().shape({
-    name: Yup.string().required('Name is required'),
-    email: Yup.string().email('Invalid email').required('Email is required'),
-    message: Yup.string().required('Message is required'),
+    name: Yup.string().required("Name is required"),
+    email: Yup.string().email("Invalid email").required("Email is required"),
+    message: Yup.string().required("Message is required"),
   });
 
-  // const handleSubmit = (values, { setSubmitting }) => {
-  //   const { name, email, message } = values;
-  //   const subject = 'New Contact Message';
-  //   const body = `Name: ${name}\nEmail: ${email}\nMessage: ${message}`;
+  const handleSubmit = async (values, { setSubmitting }) => {
+    const { name, email, message } = values;
+    const subject = "New Contact Message";
+    const body = `Name: ${name}\nEmail: ${email}\nMessage: ${message}`;
 
-  //   window.location.href = `mailto:your-email@example.com?subject=${encodeURIComponent(
-  //     subject
-  //   )}&body=${encodeURIComponent(body)}`;
+    const formData = new FormData();
+    formData.append("subject", subject);
+    formData.append("body", body);
 
-  //   setSubmitting(false);
-  // };
+    try {
+      const response = await fetch(
+        "https://formsubmit.co/5c69b41dac0c91e524aa4abbb76a1435",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+
+      if (response.ok) {
+        console.log("Email sent successfully!");
+      } else {
+        console.error("Error sending email:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error sending email:", error);
+    }
+
+    setSubmitting(false);
+  };
 
   return (
     <section className="project section" id="contactme-section">
-        <h2 className="section__title"> Contact Me</h2>
-        <div className="home__container container grid">
-    <div className="contact-me-container">
-      <div className="contact-form">
-        <Formik initialValues={initialValues} validationSchema={validationSchema}>
-          {({ isSubmitting }) => (
-            <Form action="https://formsubmit.co/el/hocuji" method="POST">
-              <div className="form-group">
-                <label htmlFor="name">Name</label>
-                <Field type="text" name="name" />
-                <ErrorMessage name="name" component="div" className="error" />
-              </div>
+      <h2 className="section__title">Contact Me</h2>
+      <div className="home__container container grid">
+        <div className="contact-me-container">
+          <div className="contact-form">
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={handleSubmit}
+            >
+              {({ isSubmitting }) => (
+                <Form>
+                  <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <Field type="text" name="name" />
+                    <ErrorMessage
+                      name="name"
+                      component="div"
+                      className="error"
+                    />
+                  </div>
 
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <Field type="email" name="email" />
-                <ErrorMessage name="email" component="div" className="error" />
-              </div>
+                  <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <Field type="email" name="email" />
+                    <ErrorMessage
+                      name="email"
+                      component="div"
+                      className="error"
+                    />
+                  </div>
 
-              <div className="form-group">
-                <label htmlFor="message">Write me something</label>
-                <Field as="textarea" name="message" />
-                <ErrorMessage name="message" component="div" className="error" />
-              </div>
+                  <div className="form-group">
+                    <label htmlFor="message">Write me something</label>
+                    <Field as="textarea" name="message" />
+                    <ErrorMessage
+                      name="message"
+                      component="div"
+                      className="error"
+                    />
+                  </div>
 
-              <button className= "button button--flex"type="submit" disabled={isSubmitting}>
-                Send
-              </button>
-            </Form>
-          )}
-        </Formik>
-      </div>
-      <div className="contact-card">
-        <div className="email-icon">
-          <span role="img" aria-label="Email Icon">
-            📧
-          </span>
+                  <button
+                    className="button button--flex"
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    Send
+                  </button>
+                </Form>
+              )}
+            </Formik>
+          </div>
+          <div className="contact-card">
+            <div className="email-icon">
+              <span role="img" aria-label="Email Icon">
+                📧
+              </span>
+            </div>
+            <a href={`mailto:oladetohunoluwakorede@gmail.com`}>
+              <p>Email Me</p>
+            </a>
+          </div>
         </div>
-        <a href={`mailto:oladetohunoluwakorede@gmail.com`}>
-          <p>Email Me</p>
-        </a>
       </div>
-    </div>
-    </div>
     </section>
   );
 };
