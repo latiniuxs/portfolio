be60927ff6bc03d18fd38e650ff293eaf4fb23f2
latiniuxs <koredeoladetohun@gmail.com>
2023-09-26 19:35:59 +0100

    changes

---
+++ src/Data/projectsData.jsx
@@ -0,0 +1,36 @@
+const projects = [
+  {
+    title: "MetaBnB",
+    description: "Responsive UI for a metaverse housing marketplace",
+    preview: "/assets/metabnb.jpg", 
+    link: "https://oladetohun1.github.io/metabnb/", 
+    technologies: ["React"],
+  },
+  {
+    title: "Image Gallery",
+    description:
+      "This project is a fully functional and responsive image gallery built using React, Vite, and Tailwind CSS. The gallery allows users to log in, view a collection of images, and interact with them using drag-and-drop functionality.",
+    preview: "/assets/imageGallery.jpg",
+    link: "https://image-gallery-sepia-seven.vercel.app",
+    technologies: ["React", "Firebase"],
+  },
+  {
+    title: "MovieBox",
+    description:
+      "A movie discovery web application that allows users to search for movies, view details about them, and save their favorite movies.",
+    preview: "/assets/MovieBox.jpg",
+    link: "https://stage-two-henna.vercel.app/",
+    technologies: ["React", "Api"],
+  },
+//   {
+//     title: "Webwing",
+//     description:
+//       "A Digital Agency Web project: a team project created by the the members of Geek Hive Community to enhance the growth, skill and confidence of every member of this community",
+//     preview: "/assets/webwing.jpg",
+//     link: "https://web-wing-geekhive-team-project-one.netlify.app",
+//     technologies: ["Javascript"],
+//   },
+];
+
+
+export default projects;
--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,39 +1,5 @@
-import React from "react";
 import "./Project.css";
-
-const projects = [
-  {
-    title: "MetaBnB",
-    description: "Responsive UI for a metaverse housing marketplace",
-    preview: "/assets/metabnb.jpg", 
-    link: "https://oladetohun1.github.io/metabnb/", 
-    technologies: ["React"],
-  },
-  {
-    title: "Image Gallery",
-    description:
-      "This project is a fully functional and responsive image gallery built using React, Vite, and Tailwind CSS. The gallery allows users to log in, view a collection of images, and interact with them using drag-and-drop functionality.",
-    preview: "/assets/imageGallery.jpg",
-    link: "https://image-gallery-sepia-seven.vercel.app",
-    technologies: ["React", "Firebase"],
-  },
-  {
-    title: "MovieBox",
-    description:
-      "A movie discovery web application that allows users to search for movies, view details about them, and save their favorite movies.",
-    preview: "/assets/MovieBox.jpg",
-    link: "https://stage-two-henna.vercel.app/",
-    technologies: ["React", "Api"],
-  },
-  {
-    title: "Webwing",
-    description:
-      "A Digital Agency Web project: a team project created by the the members of Geek Hive Community to enhance the growth, skill and confidence of every member of this community",
-    preview: "/assets/webwing.jpg",
-    link: "https://web-wing-geekhive-team-project-one.netlify.app",
-    technologies: ["Javascript"],
-  },
-];
+import projects from '../Data/projectsData'
 
 const Project = () => {
   return (
@@ -44,7 +10,7 @@ const Project = () => {
           {projects.map((project, index) => (
             <div key={index} className="project-card">
               <img
-                src={project.preview} // Display the project preview image
+                src={project.preview} 
                 alt={project.title}
                 className="project-preview"
               />
