080b50ea9202e3c29fec3bbd2441ef4a2096be64
latiniuxs <koredeoladetohun@gmail.com>
2024-01-14 00:59:17 +0100

    updated cv

--- src/Data/projectsData.jsx
+++ src/Data/projectsData.jsx
@@ -2,14 +2,14 @@ const projects = [
   {
     title:"FarmFortify",
     description:"A web application built collaboratively for farmers ",
-    preview:"./assets/Farm.jpg",
+    preview:"/assets/Farm.jpg",
     link:"https://farmfortify.com/",
     technologies:["React"],
   },
   {
     title: "MetaBnB",
     description: "Responsive UI for a metaverse housing marketplace",
-    preview: "./assets/metabnb.jpg", 
+    preview: "/assets/metabnb.jpg", 
     link: "https://oladetohun1.github.io/metabnb/", 
     technologies: ["React"],
   },
@@ -23,7 +23,7 @@ const projects = [
   {
     title:"Todo App",
     description:"a todo app that helps you to manage tasks at hand. it has features such as drag-and-drop functionality for  task organization. Create, edit, and delete tasks with a click, and re-arrange them",
-    preview:"./assets/TODO.jpg",
+    preview:"/assets/TODO.jpg",
     link: "https://todo-orcin-eta.vercel.app",
     technologies:["React"," react-dnd", "tailwindcss"],
   },
@@ -31,7 +31,7 @@ const projects = [
     title: "Image Gallery",
     description:
       "This project is a fully functional and responsive image gallery built using React, Vite, and Tailwind CSS. The gallery allows users to log in, view a collection of images, and interact with them using drag-and-drop functionality.",
-    preview: "./assets/imageGallery.jpg",
+    preview: "/assets/imageGallery.jpg",
     link: "https://image-gallery-sepia-seven.vercel.app",
     technologies: ["React", "Firebase"],
   },
@@ -40,7 +40,7 @@ const projects = [
     title: "MovieBox",
     description:
       "A movie discovery web application that allows users to search for movies, view details about them, and save their favorite movies.",
-    preview: "./assets/MovieBox.jpg",
+    preview: "/assets/MovieBox.jpg",
     link: "https://stage-two-henna.vercel.app/",
     technologies: ["React", "Api"],
   },
@@ -48,7 +48,7 @@ const projects = [
     title: "Webwing",
     description:
       "A Digital Agency Web project: a team project created by the the members of Geek Hive Community to enhance the growth, skill and confidence of every member of this community",
-    preview: "./assets/webwing.jpg",
+    preview: "/assets/webwing.jpg",
     link: "https://web-wing-geekhive-team-project-one.netlify.app",
     technologies: ["Javascript"],
   },
