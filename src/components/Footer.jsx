3dc7b24a64848c95da2a224cd6f1c32f8e796f5f
latiniuxs <140047084+latiniuxs@users.noreply.github.com>
2024-01-14 09:34:28 +0100

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import './footer.css'
+
+
+
+const Footer = () => {
+
+  const currentYear = new Date().getFullYear();
+
+
+
+  return (
+
+    <footer className="footer">
+
+      <p>
+
+        <a href="https://github.com/latiniuxs" target="_blank" rel="noopener noreferrer">
+
+          by Latiniux
+
+        </a>
+
+        &copy; {currentYear} All rights reserved.
+
+      </p>
+
+    </footer>
+
+  );
+
+};
+
+
+
+export default Footer;
